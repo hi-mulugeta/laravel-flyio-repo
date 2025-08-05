167e3836362cc0db0e3a449514191e11b492cf18
Mulugeta Mehari <debebefekadu14@gmail.com>
2025-08-05 14:56:33 +0300

    fly

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 text-gray-800">
+<div class="text-blue-500 text-2xl">
+    About page with Tailwind!
+</div>
+</body>
+</html>
